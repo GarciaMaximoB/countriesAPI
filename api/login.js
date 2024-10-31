d2ef1b1fc0e9684948fe5f11700fee20f8e2f914
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-10-31 17:31:28 -0300

    codigo de vercel

--- api/login.js
+++ api/login.js
@@ -1,21 +1,18 @@
-const express = require("express");
-const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const app = express();
-const PORT = 3000;
 
-app.use(cors());
-app.use(express.json());
-
-const usersFilePath = path.join(__dirname, "../data/users.json");
+const usersFilePath = path.join(process.cwd(), "data", "users.json");
 
 function readUsersFromFile() {
   const data = fs.readFileSync(usersFilePath);
   return JSON.parse(data);
 }
 
-app.post("/login", (req, res) => {
+module.exports = (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Método no permitido" });
+  }
+
   const { username } = req.body;
   if (!username) {
     return res.status(400).json({ error: "Falta el parámetro username" });
@@ -29,8 +26,4 @@ app.post("/login", (req, res) => {
   } else {
     res.status(404).json({ error: "Usuario no encontrado" });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+};
